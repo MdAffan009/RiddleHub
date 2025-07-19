d13d7649f72a98e6dbd954681472ca8b0297284c
Robin Parker <Mdaffan0502@gmail.com>
2025-07-19 12:14:47 +0530

    Improved Static Signup form

--- Client/src/components/SignIn/SignIn.tsx
+++ Client/src/components/SignIn/SignIn.tsx
@@ -1,22 +1,112 @@
-import { VStack } from "@chakra-ui/react";
+import { Button, Box, Field, Flex, Input, InputGroup, Stack, Text } from "@chakra-ui/react"
+import { LuUser } from "react-icons/lu"
+import { RiMailLine } from "react-icons/ri"
+import { PasswordInput } from "../../ui/password-input"
+import { type FieldValues, useForm } from "react-hook-form";
+import { z } from 'zod';
+import { zodResolver } from "@hookform/resolvers/zod";
+
+
+const schema = z.object({
+  name: z.string().min(3, { message: "Name must be at least 3 characters." }),
+  username: z.string().min(3, { message: "Username must be at least 3 characters." }),
+  email: z.email(),
+  password: z.string().min(8, { message: "Password must be at least 8 characters long." }),
+});
+
+type FormData = z.infer<typeof schema>;
+
+
 
 const SignIn = () => {
 
+  const { register, handleSubmit, formState: { errors } } = useForm<FormData>({ resolver: zodResolver(schema) });
+
+  const onSubmit = (data: FieldValues) => {
+    console.log(data);
+  }
+
+
   return (
     <>
-      <VStack>
 
-        <form>
+      <form onSubmit={handleSubmit(onSubmit)}>
 
-          <div className="mb-3">
-            <label htmlFor="name" className="form-lable">Name</label>
-            <input />
+        <Stack justifyContent="center" marginX={60} gapY={8}>
 
-          </div>
+          <Box>
 
-        </form>
+            <Field.Root>
 
-      </VStack>
+              <Field.Label>Name</Field.Label>
+              <Input {...register("name")} placeholder="Enter your Name" borderRadius={20} />
+
+            </Field.Root>
+
+            <Text color="red.400" fontSize="sm" minHeight="1.5rem">
+              {errors.name?.message ?? ""}
+            </Text>
+
+          </Box>
+
+          <Box>
+
+            <Field.Root>
+
+              <Field.Label>Username</Field.Label>
+
+              <InputGroup startElement={<LuUser />}>
+                <Input {...register("username")} placeholder="Username" borderRadius={20} />
+              </InputGroup>
+
+            </Field.Root>
+
+            <Text color="red.400" fontSize="sm" minHeight="1.5rem">
+              {errors.username?.message ?? ""}
+            </Text>
+
+          </Box>
+
+          <Box>
+
+            <Field.Root>
+
+              <Field.Label>Email</Field.Label>
+
+              <InputGroup startElement={<RiMailLine />}>
+                <Input {...register("email")} type="email" placeholder="Enter your Email" borderRadius={20} />
+              </InputGroup>
+
+            </Field.Root>
+
+            <Text color="red.400" fontSize="sm" minHeight="1.5rem">
+              {errors.email?.message ?? ""}
+            </Text>
+
+          </Box>
+
+          <Box>
+
+            <Field.Root>
+
+              <Field.Label>Password</Field.Label>
+              <PasswordInput {...register("password")} borderRadius={20} placeholder="Enter your password" />
+
+            </Field.Root>
+
+            <Text color="red.400" fontSize="sm" minHeight="1.5rem">
+              {errors.password?.message ?? ""}
+            </Text>
+
+          </Box>
+
+
+          <Flex justifyContent={"center"}>
+            <Button type="submit" width="150px" borderRadius={20}>Submit</Button>
+          </Flex>
+
+        </Stack>
+      </form>
 
     </>
   )
--- Client/src/main.tsx
+++ Client/src/main.tsx
@@ -1,12 +1,12 @@
 import { Provider } from './ui/provider.tsx';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
+import App from './App';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider>
       <App />
     </Provider>
-  </StrictMode>,
+  </StrictMode>
 )
